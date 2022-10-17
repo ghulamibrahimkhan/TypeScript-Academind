ec88fef672ca57e491bb9e2a8b4b472f251aa7d4
ghulamibrahimkhan <gulamibrahimkhan@gmail.com>
2022-10-17 04:51:05 -0700

    numbers,string,bool

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-var x = 1;
-var y = 2;
-var z = 3;
-console.log(x + y + z);
+function add(num1, num2, resStatus, phrase) {
+    var result = num1 + num2;
+    if (resStatus) {
+        return phrase + result;
+    }
+}
+var n1 = 3;
+var n2 = 6;
+var resStatus = true;
+var phrase = "Result is: ";
+var result = add(n1, n2, resStatus, phrase);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,4 +1,13 @@
-const x = 1;
-const y = 2;
-const z = 3;
-console.log(x+y+z);
+function add(num1: number, num2: number, resStatus: boolean, phrase: string) {
+    let result = num1+num2;
+    if (resStatus) {
+        return phrase + result
+    }
+}
+const n1 = 3;
+const n2 = 6;
+const resStatus = true;
+const phrase = "Result is: "
+
+const result = add(n1,n2,resStatus, phrase)
+console.log(result);
